cabafd28de85f806ff0ca770306bdbf66e37b4b4
William Wold <wm@wmww.sh>
2019-01-16 03:49:29 -0500

    Add wayland-layer-shell-gtk abstraction

--- mate-panel/wayland-backend.h
+++ mate-panel/wayland-backend.h
@@ -9,13 +9,13 @@
 
 #include <gdk/gdk.h>
 #include <gdk/gdkwayland.h>
-#include <gtk/gtk.h>
 
 #include "panel-toplevel.h"
 
+// If we are currently running Wayland
+// Implemented in wayland-layer-shell.c
 gboolean is_using_wayland (void);
 
-// wayland_* functions must only be called when using Wayland
 void wayland_init (void);
 void wayland_panel_toplevel_realize (PanelToplevel *window);
 void wayland_panel_toplevel_set_strut (PanelToplevel     *gdk_window,
